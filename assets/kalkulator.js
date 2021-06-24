864d5ed62d4a5c4eef0c997fab93823eeb66f90d
alifdevz <alifm.devz@gmail.com>
2021-06-24 20:58:36 +0800

    Added an array and add comments

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,1 +1,6 @@
+// Test Javascript
 console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+
+// Array
+let myArray = ["Coklat", 42.5, 22, true, "Programming"];
+console.log(myArray);
