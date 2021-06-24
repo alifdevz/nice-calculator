69ae8ad3a210e6650f294fd08d7940216f8a133c
alifdevz <alifm.devz@gmail.com>
2021-06-24 21:05:15 +0800

    Accessing elements of the array

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -4,3 +4,12 @@ console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
 // Array
 let myArray = ["Coklat", 42.5, 22, true, "Programming"];
 console.log(myArray);
+
+// Akses isi dalam array
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]);
+console.log(myArray[5]);  // bernilai "undefined" karena array diakses melebihi indexnya
+console.log("Panjang nilai myArray adalah " + myArray.length + ".");
