21edb7c0fde71d812cdc82cede096968e3eafc8b
alifdevz <alifm.devz@gmail.com>
2021-06-28 21:26:30 +0800

    Added two functions, updateDisplay() and clearCalculator()

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -4,3 +4,14 @@ const calculator = {
     firstNumber: null,
     waitingForSecondNumber: false
 }
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
