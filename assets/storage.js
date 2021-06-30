21eb744072a4d1398e12fcd9d98b6729d6acf43a
alifdevz <alifm.devz@gmail.com>
2021-06-30 12:02:30 +0800

    Populate storage.js code to save and show calculations history using storage.js

---
+++ assets/storage.js
@@ -0,0 +1,33 @@
+const CACHE_KEY = "calculation_history";
+
+function checkForStorage() {
+    return typeof(Storage) !== "undefined";
+}
+
+function showHistory() {
+    if (checkForStorage()) {
+        return JSON.parse(localStorage.getItem(CACHE_KEY)) || [];
+    } else {
+        return [];
+    }
+}
+
+function renderHistory() {
+    const historyData = showHistory();
+    let historyList = document.querySelector("#historyList");
+
+    // selalu hapus konten HTML pada elemen historyList agar tidak menampilkan data ganda
+    historyList.innerHTML = "";
+
+    for (let history of historyData) {
+        let row = document.createElement('tr');
+        row.innerHTML = "<td>" + history.firstNumber + "</td>";
+        row.innerHTML += "<td>" + history.operator + "</td>";
+        row.innerHTML += "<td>" + history.secondNumber + "</td>";
+        row.innerHTML += "<td>" + history.result + "</td>";
+  
+        historyList.appendChild(row);
+    }
+}
+
+renderHistory();
