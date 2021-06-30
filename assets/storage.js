e35767f39e10d237b51e4c519ccbbebc34da0af5
alifdevz <alifm.devz@gmail.com>
2021-06-30 12:09:28 +0800

    Added the forgotten putHistory() function

--- assets/storage.js
+++ assets/storage.js
@@ -4,6 +4,25 @@ function checkForStorage() {
     return typeof(Storage) !== "undefined";
 }
 
+function putHistory(data) {
+    if (checkForStorage()) {
+        let historyData = null;
+        if (localStorage.getItem(CACHE_KEY) === null) {
+            historyData = [];
+        } else {
+            historyData = JSON.parse(localStorage.getItem(CACHE_KEY));
+        }
+  
+        historyData.unshift(data);
+  
+        if (historyData.length > 5) {
+            historyData.pop();
+        }
+  
+        localStorage.setItem(CACHE_KEY, JSON.stringify(historyData));
+    }
+ } 
+
 function showHistory() {
     if (checkForStorage()) {
         return JSON.parse(localStorage.getItem(CACHE_KEY)) || [];
